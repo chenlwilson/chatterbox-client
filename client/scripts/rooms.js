650051bdfad908c12b0c59cf4909127b5cef71a6
Chen Wilson <chenlwilson@gmail.com>
2018-11-07 21:10:16 -0800

    Complete rooms

--- client/scripts/rooms.js
+++ client/scripts/rooms.js
@@ -1,5 +1,6 @@
 var Rooms = {
 
+    //add a new room option to the select dropdown
     add: function(roomname) {
       var template = _.template(`
         <option value ='<%= roomname %>'><%= roomname %></option>
@@ -7,6 +8,9 @@ var Rooms = {
       RoomsView.$select.append(template({roomname: roomname}));
     },
 
+    //when a new room is created
+    //send the new room name to the server
+    //send the first message created in this room
     submitNewRoom: function(roomname) {
       Parse.create({
         username: App.username,
--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -3,35 +3,40 @@ var RoomsView = {
   $button: $('#rooms button'),
   $select: $('#rooms select'),
 
-  //this function renders whatever is written in the renderRoom
-  //input is what is selected (the dropdown of rooms)
   initialize: function() {
-    //event handler that manipulates button upon click
-    //refers to line 3
+    //event handler that add a new room when clicked
     RoomsView.$button.on('click', RoomsView.handleAddButton);
-    //event handler that changes the room
+    //event handler that changes message display based on the room selected
     RoomsView.$select.on('change', RoomsView.selectRoom);
   },
 
   handleAddButton: function() {
+    //the new roomname is the input of the prompt
     var roomname = prompt('Enter a new room')
     Rooms.add(roomname);
     Rooms.submitNewRoom(roomname);
   },
 
   selectRoom: function() {
+    //when a room is selected
+    //fetch data from server
+    //render only messages with the selected roomname
     Parse.readAll((data) => {
       RoomsView.renderRoom(RoomsView.$select.val(), data)
     });
   },
 
   uniqRooms: function(data) {
+    //create a list of unique rooms for rendering in the select dropdown
     return _.uniq(_.map(data, function(message) {
       return message.roomname;
     }));
   },
 
   renderRoom: function(roomname, data) {
+    //first clear all children div in #chats
+    //if Lobby is selected, display all messages
+    //if a roomname is selected, display messages with the selected roomname
     $('#chats').empty();
     if(roomname === 'Lobby') {
       for(var i=0; i<data.results.length; i++) {
