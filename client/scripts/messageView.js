57c1c37f43d123853e79350f2a7e00f664e7afa6
Chen Wilson <chenlwilson@gmail.com>
2018-11-07 22:14:28 -0800

    Complete Bare Minimum Requirements

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -1,24 +1,16 @@
 var MessageView = {
-//goal: template function that renders message to html
-  //render the username of the message
-  //append to dom
-  // .append()
-  // ex) $( ".inner" ).append( "<p>Test</p>" );
+//template function that renders message to html
+// ex) $( ".inner" ).append( "<p>Test</p>" );
 
-  //var compiled = _.template("hello: <%= name %>");
-  // compiled({name: 'moe'});
-  // => "hello: moe"
+//underscore _.template syntax:
+//var compiled = _.template("hello: <%= name %>");
+// compiled({name: 'moe'});
+// => "hello: moe"
 
-  // Josh's Notes:
-  // key is variable (hello)
-  // input is the whole object you want to put in
-  // the arguement is the value of the key we made
-  // render gets the value of these keys
+//input is an object
+//key is the variable name, value is the arg passed
+//e.g: _.template({username: 'anonymous'})
 
-  // Changes we made:
-  // added line 26 (copied from line 25 og)
-  // changed message to username (25)
-  // changed message to text (26)
   render: _.template(`
       <div class="chat">
         <div class="username <%= username %>"><%= username %></div>
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -7,10 +7,6 @@ var MessagesView = {
     MessagesView.$chats.on('submit', MessagesView.renderMessage);
   },
 
-  render: function() {
-
-  },
-
   renderMessage: function(message) {
     //uses escaping algorithm placed on app.js
     //converts username characters to a string for
@@ -21,9 +17,5 @@ var MessagesView = {
 
     //renders "cleaned up" message username & text
     this.$chats.append(MessageView.render(message));
-  },
-    //we have message as a raw data that's not escaped
-    //we want to escape username and text
-
-
+  }
 };
